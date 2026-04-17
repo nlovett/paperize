8eba44c3e1fb1f33e6a61df197284de95c2e2bb4
nlovett <nlovett@gmail.com>
2026-04-17 13:22:17 -0400

    add argparse

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import argparse
+
+
 def main():
-    print("Hello from paperize!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--message", "-m", default="Hello from paperize!")
+    args = parser.parse_args()
+    print(args.message)
 
 
 if __name__ == "__main__":
